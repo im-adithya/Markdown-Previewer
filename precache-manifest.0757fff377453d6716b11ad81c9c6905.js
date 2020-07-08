8f29e8954887cb794d51e6a3e8f9f1593a4ad081
im-adithya <imadithyavardhan@gmail.com>
2020-07-08 19:49:21 +0530

    Updates

---
+++ precache-manifest.0757fff377453d6716b11ad81c9c6905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b538ecef9343a6b46461c41db5567d1e",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "fd758768fcfc818100d4",
+    "url": "/Markdown-Previewer/static/js/2.a512570a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Markdown-Previewer/static/js/2.a512570a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e440837629d21c616e",
+    "url": "/Markdown-Previewer/static/js/main.8a61e887.chunk.js"
+  },
+  {
+    "revision": "b0a69d01708cd88e3db1",
+    "url": "/Markdown-Previewer/static/js/runtime-main.baa23fdd.js"
+  }
+]);
